45caffe21e5966e21d2eab1ea8cc78faeb508f39
anubha11 <31592453+anubha11@users.noreply.github.com>
2018-06-17 15:37:22 +0530

    Create Conditional Statements: Switch.js

---
+++ Day 2/Conditional Statements: Switch.js
@@ -0,0 +1,54 @@
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch (s[0]){
+        case 'a':
+            letter="A";
+            break;
+        case 'e':
+            letter="A";
+            break;
+        case 'i':
+            letter="A";
+            break;
+        case 'o':
+            letter="A";
+            break;
+        case 'u':
+            letter="A";
+            break;
+        case 'b':
+            letter="B";
+            break;
+        case 'c':
+            letter="B";
+            break;
+        case 'd':
+            letter="B";
+            break;
+        case 'f':
+            letter="B";
+            break;
+        case 'g':
+            letter="B";
+            break;
+        case 'h':
+            letter="C";
+            break;
+        case 'j':
+            letter="C";
+            break;
+        case 'k':
+            letter="C";
+            break;
+        case 'l':
+            letter="C";
+            break;
+        case 'm':
+            letter="C";
+            break;
+        default:
+            letter="D";
+    }
+    return letter;
+}
